402b8e957286b8e210da365485879dd273a5d011
Heitor Filho <heitor.filho@ufu.br>
2024-02-04 19:27:46 -0300

    PrintWriter and BufferedReader

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,4 +1,7 @@
+//import java.io.BufferedWriter;
 import java.io.IOException;
+//import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -7,6 +10,7 @@ public class ChatClient {
     private final String SERVER_ADDRESS = "127.0.0.1";
     private Socket clienSocket;
     private Scanner scanner;
+    private PrintWriter out;
 
     public ChatClient(){
         scanner = new Scanner(System.in);
@@ -14,16 +18,29 @@ public class ChatClient {
 
     public void start() throws IOException{
         clienSocket = new Socket(SERVER_ADDRESS, ChatServer.PORT);
-        clienSocket.getOutputStream(); // fluxo de saida de dados
+
+        //this.out = new BufferedWriter(new OutputStreamWriter(clienSocket.getOutputStream()));
+        // OutputStream - byte []
+        // OutputStreamWriter - char []
+        // BufferedWriter - String + nextline()
+
+        this.out = new PrintWriter(clienSocket.getOutputStream(), true);
+
         System.out.println("Cliente conectado ao servidor em: " + SERVER_ADDRESS + ":" + ChatServer.PORT);
         messageLoop();
     }
 
-    private void messageLoop(){
+    private void messageLoop() throws IOException{
         String msg;
         do {
             System.out.print("Digite uma mensagem (ou sair para finalizar): ");
             msg = scanner.nextLine();
+
+            //out.write(msg); BufferedWriter
+            //out.newLine(); BufferedWriter
+
+            out.println(msg);
+            //out.flush(); // garante que todo dado do buffer foi escrito, com o autoflush:true se torna automatico a cada println
 
         } while (!msg.equalsIgnoreCase("sair"));
     }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,6 +19,13 @@ public class ChatServer {
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Cliente: " + clientSocket.getRemoteSocketAddress() + " conectou");
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String msg = in.readLine();
+            System.out.println(
+                "Mensagem recebida do cliente " + clientSocket.getRemoteSocketAddress()
+                 + ": "+ msg);
         }
     }
 
